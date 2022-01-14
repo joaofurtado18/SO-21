cb957791f1992d00778f58a0e542eef8de58e4eb
Guilherme Carabalone <guilherme.caraba@gmail.com>
2022-01-14 13:54:57 -0300

    FALTAM SÓ OS TESTES

--- tecnicofs/tests/test1.c
+++ tecnicofs/tests/test1.c
@@ -18,13 +18,13 @@ int main() {
 
     char *str = "6UPl5ldgtEBUGL2AAAAAAAAAHrjmDrpVwDcEb57s3hqCa4HW8zMPGSeZA4mjjqYTfU99hADCOJU7XhwgvkxPeEm4MoeomSUZTFjiuHYvKrtNVI5E6SACcx85IWbkqn118z2qtbeFHbx9f5YTHiq4uGNGj43Z33TP35ERTbNhu4kYMch9gRwmYUHjJ7FgPWwmPc3tA71hk5pCdULrzJcdlXitvreLNf2scKkHH5LwoAjpj0KNxVj4owJEWh0gklIPoRotyYOxBNz4weQMCaPYYr2qL8Lhrlo7v";
     char *path = "/f1";
-    /*char buffer[300];*/
+    char buffer[300];
     args arg;
     assert(tfs_init() != -1);
     puts("testesgseges");
 
     int f;
-    /*ssize_t r;*/
+    ssize_t r;
     pthread_t thread;
     puts("e");
     f = tfs_open(path, TFS_O_CREAT);
@@ -36,15 +36,15 @@ int main() {
 
     f = tfs_close(f);
 
-    /*tfs_open(path, 0);
+    tfs_open(path, 0);
     r = tfs_read(f, buffer, sizeof(buffer) - 1);
 
-    buffer[r] = '\0';*/
+    buffer[r] = '\0';
 
 
     printf("Successful test.\n");
 
-    /*tfs_copy_to_external_fs(path, "test1.txt");*/
+    tfs_copy_to_external_fs(path, "test1.txt");
 
 
     return 0;
